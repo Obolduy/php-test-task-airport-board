91f5aad668e32cb654a95a1159323716d9ecf58d
Obolduy <ivanox01@yandex.ru>
2022-02-16 21:22:08 +0300

    Метод calculateTZDifference в Flight

--- src/Entity/Flight.php
+++ src/Entity/Flight.php
@@ -37,9 +37,29 @@ class Flight
         return $this->toTime;
     }
 
-    public function calculateDurationMinutes(): int
+    /**
+     * Высчитывает разницу между часовыми поясами.
+     * @param string $fromTZ Часовой пояс аэропорта вылета.
+     * @param string $toTZ Часовой пояс аэропорта назначения.
+     * @return int Разность часовых поясов.
+     */
+    private function calculateTZDifference(string $fromTZ, string $toTZ): int
     {
-        return $this->calculateMinutesFromStartDay($this->toTime) - $this->calculateMinutesFromStartDay($this->fromTime);
+        $toTZ = (int)substr($toTZ, -3, 3);
+        $fromTZ = (int)substr($fromTZ, -3, 3);
+
+        // Из-за того, что мы находим разность TZ, мы должны считать модули чисел
+        $toTZ = ($toTZ > 0) ? $toTZ : -$toTZ;
+        $fromTZ = ($fromTZ > 0) ? $fromTZ : -$fromTZ;
+
+        $difference = 60 * ($toTZ - $fromTZ);
+
+        // Если разность TZ не попадает в промежуток от 720 до -720,
+        // мы должны уменьшить или увеличить число на 720 минут (12 часов).
+        if ($difference > 720) $difference -= 720;
+        if ($difference < -720) $difference += 720;
+
+        return $difference;
     }
 
     private function calculateMinutesFromStartDay(string $time): int
